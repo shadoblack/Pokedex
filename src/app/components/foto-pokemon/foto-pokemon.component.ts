07cdaf43ec44ed2c00f7d01d5adb0c37fadc677a
EdgarRB <edgarerb@hotmail.es>
2023-09-20 14:23:44 +0200

    cuerpo y llamadas API basicas de detalle

--- src/app/components/foto-pokemon/foto-pokemon.component.ts
+++ src/app/components/foto-pokemon/foto-pokemon.component.ts
@@ -1,13 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { Pokemon } from 'src/app/interfaces/pokemon';
 
 @Component({
   selector: 'app-foto-pokemon',
   templateUrl: './foto-pokemon.component.html',
-  styleUrls: ['./foto-pokemon.component.scss']
+  styleUrls: ['./foto-pokemon.component.scss'],
 })
 export class FotoPokemonComponent {
-
-  @Input() pokemon?:Pokemon
-
+  @Input() pokemon?: Pokemon;
 }
